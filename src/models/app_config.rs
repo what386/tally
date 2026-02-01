c4ba1c5f23c6e6af48bd857b3e5b0d68d5a76e64
bmorin <robot0324@gmail.com>
2026-02-01 16:46:25 -0500

    config

--- src/models/app_config.rs
+++ src/models/app_config.rs
@@ -1,4 +1,64 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, Default)]
-pub struct AppConfig {}
+use crate::models::common::Priority;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AppConfig {
+    defaults: Defaults,
+    git: Git,
+    commands: Prune,
+
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Defaults {
+    priority: Priority,
+    tags: Vec<String>,
+    editor: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Git {
+    done_prefix: String
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Prune {
+    prune_after_days: i64,
+}
+
+impl Default for AppConfig {
+    fn default() -> Self {
+        Self {
+            defaults: Defaults::default(),
+            git: Git::default(),
+            commands: Prune::default(),
+        }
+    }
+}
+
+impl Default for Defaults {
+    fn default() -> Self {
+        Self {
+            priority: Priority::Medium,
+            tags: Vec::new(),
+            editor: "nvim".to_string(),
+        }
+    }
+}
+
+impl Default for Git {
+    fn default() -> Self {
+        Self {
+            done_prefix: "done:".to_string(),
+        }
+    }
+}
+
+impl Default for Prune {
+    fn default() -> Self {
+        Self {
+            prune_after_days: 30,
+        }
+    }
+}
